a78bb1cb4bc907391e178b4f8484118873284252
Victar Nikitsin <nikivictor64@gmail.com>
2025-09-22 19:25:22 +0300

    feat(client): Add initial webpack configuration

---
+++ client/config/build/buildLoaders.ts
@@ -0,0 +1,38 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export const buildLoaders = (options: BuildOptions): webpack.RuleSetRule[] => {
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          esModule: false,
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: options.isDev ?
+              '[path][name]__[local]--[hash:base64:5]'
+              :
+              '[hash:base64:8]',
+          }
+        }
+      },
+      "sass-loader",
+    ],
+  }
+
+  return [
+    typescriptLoader,
+    cssLoader,
+  ]
+}
---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import './index.scss'
+
+const App = () => {
+  return (
+    <div className='app'>
+    </div>
+  );
+};
+
+export default App;
