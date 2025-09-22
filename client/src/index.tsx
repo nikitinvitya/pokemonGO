d16371b279d4351ac5ad911480e6bb4a92c97ea5
Victar Nikitsin <nikivictor64@gmail.com>
2025-09-22 20:28:47 +0300

    feat(theme): Add theme provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,17 @@
 import {createRoot} from "react-dom/client";
 import App from "./App";
+import ThemeProvider from "./theme/ThemeProvider";
 
 const container = document.getElementById('root')
 
-if(!container) {
+if (!container) {
   throw new Error('root is undefined')
 }
 
 const root = createRoot(container)
 
 root.render(
-  <App/>
+  <ThemeProvider>
+    <App/>
+  </ThemeProvider>
 )
