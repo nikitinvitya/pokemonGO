a4be6a02b77d694486c9579befe2969ac847ec31
Victar Nikitsin <nikivictor64@gmail.com>
2025-09-30 01:00:23 +0300

    feat(ui): Add not found page

--- client/src/shared/config/routeConfig/routeConfig.tsx
+++ client/src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from '@/pages/MainPage';
 import { PokemonPage } from '@/pages/PokemonPage';
+import NotFoundPage from '@/pages/NotFoundPage/NotFoundPage';
 
 const enum AppRoutes {
   MAIN = 'main',
-  POKEMON_PAGE = 'pokemon',
+  POKEMON_PAGE = 'pokemon_page',
+  NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.POKEMON_PAGE]: 'pokemon/:name',
+  [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -18,7 +21,11 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     element: <MainPage />,
   },
   [AppRoutes.POKEMON_PAGE]: {
-    path: RoutePath.pokemon,
+    path: RoutePath.pokemon_page,
     element: <PokemonPage />,
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath.not_found,
+    element: <NotFoundPage />,
   },
 };
